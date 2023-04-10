80f0a775b0f4c18797e489750ab50a3ff1f5afea
unknown <contato.itamargouveia@gmail.com>
2023-04-10 12:13:53 -0300

    material ui configurado

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
-import {Routes , Route,Navigate } from 'react-router-dom'
+import { Button } from '@mui/material'
+import {Routes , Route } from 'react-router-dom'
 
 export const AppRoutes = () => {
     return(
         <Routes>
-             <Route path="/pagina-inicial" element={<p>jflaflaf</p>} />
+             <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>teste</Button>} />
              <Route path='*' element={<p>404</p>}/>
         </Routes>
     )
