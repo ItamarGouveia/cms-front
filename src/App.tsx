dc8d25869b9a3a4115fadb65ab0f9a49489971b6
unknown <contato.itamargouveia@gmail.com>
2023-04-10 15:48:23 -0300

    eslint configurado

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes } from "./routes";
-import {ThemeProvider} from '@mui/material'
-  import { LightTheme } from "./shared/themes";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+import { ThemeProvider } from '@mui/material';
+import { LightTheme } from './shared/themes';
+import { AppThemeProvider } from './shared/contexts';
 
-import { Light } from "@mui/icons-material";
+import { Light } from '@mui/icons-material';
 export const App = () => {
-  return (
-    <ThemeProvider theme={LightTheme}>
-      
-    
-    <BrowserRouter>
-   <AppRoutes/>
-    
-    </BrowserRouter>
-    </ThemeProvider>
-  );
-}
+    return (
+        <AppThemeProvider>
+            <BrowserRouter>
+                <AppRoutes />
+            </BrowserRouter>
+        </AppThemeProvider>
+    );
+};
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@mui/material'
-import {Routes , Route } from 'react-router-dom'
-
+import { Button } from '@mui/material';
+import {Routes , Route } from 'react-router-dom';
+import React from 'react';
 export const AppRoutes = () => {
     return(
         <Routes>
-             <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'>teste</Button>} />
-             <Route path='*' element={<p>404</p>}/>
+            <Route path="/" element={<Button variant='contained' color='primary'>teste</Button>} />
+            <Route path='*' element={<p>404</p>}/>
         </Routes>
-    )
-}
+    );
+};
